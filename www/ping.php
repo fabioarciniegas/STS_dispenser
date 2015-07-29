92ec3ea4e1f355d5d0905b65c11bd61e1374cf4b
Fabio Arciniegas <fab.arciniegas@gmail.com>
2015-07-29 00:15:01 +0000

    metadata and ping target for load balancing added

--- www/ping.php
+++ www/ping.php
@@ -1,1 +1,6 @@
-ping health check for load balancing. should return 200.
+Ping health check for load balancing. 
+Notice this is an explicitly different target than / for two reasons:
+
+1. Ping for load balancer should return 200. If auto-initiate config option is turned on a redirect code would be correctly sent sometimes for /, thus confusing the load balancer into thinking this is not a healthy image.
+
+2. extensibility. In future versions/customizations may perform additional tests here before returning 200.
