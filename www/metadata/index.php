d32d52e941cf8153021fdb4a5cac41e0477b0621
Ubuntu <ubuntu@ip-10-0-1-182.ec2.internal>
2015-07-30 01:06:16 +0000

    minor cosmetic changes

--- www/metadata/index.php
+++ www/metadata/index.php
@@ -2,10 +2,10 @@
 require("../../config.php"); 
 ?>
 <?xml version="1.0"?>
-<md:EntityDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata" entityID="<?php echo PUBLIC_URL?>">
+<md:EntityDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata" entityID="<?php echo SP_ID?>">
   <md:SPSSODescriptor protocolSupportEnumeration="urn:oasis:names:tc:SAML:1.1:protocol urn:oasis:names:tc:SAML:2.0:protocol">
-    <md:AssertionConsumerService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" Location="<?php echo PUBLIC_URL?>" index="0"/>
-    <md:AssertionConsumerService Binding="urn:oasis:names:tc:SAML:1.0:profiles:browser-post" Location="<?php echo PUBLIC_URL?>" index="1"/>
+    <md:AssertionConsumerService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" Location="<?php echo SP_ID?>" index="0"/>
+    <md:AssertionConsumerService Binding="urn:oasis:names:tc:SAML:1.0:profiles:browser-post" Location="<?php echo SP_ID?>" index="1"/>
   </md:SPSSODescriptor>
   <md:ContactPerson contactType="technical">
     <md:GivenName><?php echo CONTACT_PERSON_NAME?></md:GivenName>
